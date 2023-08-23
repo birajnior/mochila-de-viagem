4f5acd0174e4cbfcffb22594b83087905ccfd276
birajnior <ubiratanbernardo@gmail.com>
2023-08-23 20:28:52 -0300

    limpando o código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,45 @@
 const form = document.querySelector("#novoItem");
 const lista = document.querySelector("#lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach((element) => {
+  criaElemento(element);
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  criaElemento(
-    event.target.elements["nome"].value,
-    event.target.elements["quantidade"].value
-  );
+
+  const nome = event.target.elements["nome"];
+  const quantidade = event.target.elements["quantidade"];
+
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
+  criaElemento(itemAtual);
+
+  itens.push(itemAtual);
+
+  localStorage.setItem("itens", JSON.stringify(itens));
+
+  nome.value = "";
+  quantidade.value = "";
 });
 
-function criaElemento(nome, quantidade) {
-  console.log(nome, quantidade);
-
+function criaElemento(item) {
   const novoItem = document.createElement("li");
   novoItem.classList.add("item");
 
   const numItem = document.createElement("strong");
+
   const iconDel = document.createElement("i");
   iconDel.classList.add("fa-regular");
   iconDel.classList.add("fa-trash-can");
 
-  numItem.innerHTML = quantidade;
+  numItem.innerHTML = item.quantidade;
   novoItem.appendChild(numItem);
-  novoItem.innerHTML += nome;
+  novoItem.innerHTML += item.nome;
   novoItem.appendChild(iconDel);
+
   lista.appendChild(novoItem);
 }
